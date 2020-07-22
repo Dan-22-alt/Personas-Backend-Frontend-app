b960e33f1f4ee0aa1a7bf5ddfd3f53a904e2e357
Daniel <DANDANUS1999@gmail.com>
2020-07-22 17:53:06 -0300

    personas-backend-frontend-commit

---
+++ personas-frontend-angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PersonaService } from './persona-service';
+import { DataService } from './data-service';
+import { PersonasComponent } from './personas/personas.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormularioComponent } from './formulario/formulario.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonasComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [PersonaService,DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
